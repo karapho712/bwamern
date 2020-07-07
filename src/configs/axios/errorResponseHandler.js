1e48c56f55999b36763142b5e6140607e9cd139f
karapho712 <newhijabmuallif@gmail.com>
2020-07-08 04:29:30 +0800

    fix errorResponseHandler

--- src/configs/axios/errorResponseHandler.js
+++ src/configs/axios/errorResponseHandler.js
@@ -1,14 +1,13 @@
 function errorResponseHandler(error) {
-    if (error)
-    let message ;
-    if(error.response){
-        if (error.response.status === 500) message = "Something Went Wrong";
-        else message = error.response.message;
+  if (error) var message;
+  if (error.response) {
+    if (error.response.status === 500) message = "Something Went Wrong";
+    else message = error.response.message;
 
-        console.log(message);
-        
-        return Promise.reject(error);
-    }
+    console.log(message);
+
+    return Promise.reject(error);
+  }
 }
 
 export default errorResponseHandler;
